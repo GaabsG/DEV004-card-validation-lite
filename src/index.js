c980f7ded1174b09e518f8055ed4666cfbb2be00
Gaby <ggaabrielagomez@gmail.com>
2023-01-14 15:13:24 -0600

    cambios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-var arrayUno= "formularioTarjeta"
-function validarTarjeta() { 
-    var reversa= arrayUno.reverse();
-    
-    console.log('reversa:', reversed);
-}
+// Boton de espera 
 
-/* function aleatorio (min, max ) {
-    return Math.floor(Math.random() * (max - min + 1) + min) */
+const myButton = document.getElementById('myButton');
+ 
+myButton.addEventListener('click', function() {
+    myButton.disabled = true;
+    myButton.style.opacity = 0.7;
+    myButton.textContent = 'Ejecutando proceso...';
+
+// Validar si es un numero 
+
+function validarNumero() {
+    var numeroTarjeta = document.getElementById("cardNumber");
+    if(typeof numeroTarjeta === "number") {
+        alert("Sus datos están siendo procesados");    
+    } 
+    else {
+        alert("Solo se admiten números");   
+    }
+} 
