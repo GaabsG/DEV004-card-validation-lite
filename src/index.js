f52830337ebb180e8012af77f7fd4b9a47d69c15
Gaby <ggaabrielagomez@gmail.com>
2023-01-14 14:24:07 -0600

    js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var arrayUno= "formularioTarjeta"
+function validarTarjeta() { 
+    var reversa= arrayUno.reverse();
+    
+    console.log('reversa:', reversed);
+}
+
+/* function aleatorio (min, max ) {
+    return Math.floor(Math.random() * (max - min + 1) + min) */
